3b725658c31abd64f01881005fe42355a2a6900d
Mayank Agarwal <mayank265@gmail.com>
2020-04-23 20:22:35 +0530

    FFT Merged

--- POTS-Python/FFT_module_wise.py
+++ POTS-Python/FFT_module_wise.py
@@ -20,14 +20,14 @@ def fft_module_individual(filename_read, column_header, color_choice):
     column_to_numpy_array = np.asarray(u_list)
     slice_of_input_array_for_processing_FFT = []
 
-    percentage = float(
-        input(
-            "Enter the % number of samples for FFT Calculation (default 10 percent). Press Enter to accept default : ") or "10")
+    # percentage = float(
+    #     input(
+    #         "Enter the % number of samples for FFT Calculation (default 10 percent). Press Enter to accept default : ") or "10")
+    #
+    # frequency = int(input("Enter the sampling frequency: "))
 
-    frequency = int(input("Enter the sampling frequency: "))
-
-    # percentage = 10
-    # frequency = 100
+    percentage = 10
+    frequency = 100
 
     N = int(len(u_list) * (percentage / 100))
 
@@ -66,4 +66,82 @@ for i, j in zip(column_header, colors):
     fft_module_individual(filename_read, i, j)
 #
 
-# fft_module_merged(filename_read, column_header, colors)
+def make_arrays():
+    pass
+
+def fft_module_merged(filename_read, column_header, color_choice):
+    header = ['Freq', 'Amplitude']
+    filename_read = 'FFT_sample.csv'
+    column_header = 'u'
+    color_choice='red'
+    data_filename_write = filename_read.split('.')[0] + '_Column_Header_' + column_header + '_FFT_Data.csv'
+    image_filename_write = filename_read.split('.')[0] + '_Column_Header_' + column_header + '_FFT_Spectra_Merged.jpg'
+    df = pd.read_csv(filename_read)
+    df.dropna(inplace=True)
+
+
+    for i in column_header:
+        print(column_header)
+
+
+
+    u_list = df[column_header].tolist()
+    column_to_numpy_array = np.asarray(u_list)
+
+    column_to_numpy_array_U = np.asarray(df["u"].tolist())
+    column_to_numpy_array_V = np.asarray(df["v"].tolist())
+    column_to_numpy_array_W = np.asarray(df["w"].tolist())
+
+    slice_of_input_array_for_processing_FFT_U = []
+    slice_of_input_array_for_processing_FFT_V = []
+    slice_of_input_array_for_processing_FFT_W = []
+
+    # percentage = float(
+    #     input(
+    #         "Enter the % number of samples for FFT Calculation (default 10 percent). Press Enter to accept default : ") or "10")
+    #
+    # frequency = int(input("Enter the sampling frequency: "))
+
+    percentage = 10
+    frequency = 100
+
+    N = int(len(u_list) * (percentage / 100))
+
+    # num_samples = 3000
+    for i in range(0, N):
+        slice_of_input_array_for_processing_FFT_U.append(column_to_numpy_array_U[i])
+        slice_of_input_array_for_processing_FFT_V.append(column_to_numpy_array_V[i])
+        slice_of_input_array_for_processing_FFT_W.append(column_to_numpy_array_W[i])
+
+    # frequency of signal
+    T = 1 / frequency
+    # x=np.linspace(0,N*T,N)#0, ,21
+    y = slice_of_input_array_for_processing_FFT_U
+    ####### processs via window  y = windowing(y)
+    yf = fft(y)
+    xf = np.linspace(0.0, 1.0 / (2 * T), N // 2)  # 0, ,10
+    my_list = 2.0 / N * np.abs(yf[0:N // 2])
+    plt.loglog(xf, 2.0 / N * np.abs(yf[0:N // 2]), color="r")
+
+    y = slice_of_input_array_for_processing_FFT_V
+    ####### processs via window  y = windowing(y)
+    yf = fft(y)
+    xf = np.linspace(0.0, 1.0 / (2 * T), N // 2)  # 0, ,10
+    my_list = 2.0 / N * np.abs(yf[0:N // 2])
+    plt.loglog(xf, 2.0 / N * np.abs(yf[0:N // 2]), color="g")
+
+    y = slice_of_input_array_for_processing_FFT_W
+    ####### processs via window  y = windowing(y)
+    yf = fft(y)
+    xf = np.linspace(0.0, 1.0 / (2 * T), N // 2)  # 0, ,10
+    my_list = 2.0 / N * np.abs(yf[0:N // 2])
+    plt.loglog(xf, 2.0 / N * np.abs(yf[0:N // 2]), color="b")
+
+    plt.grid()
+    plt.xlabel('Frequency')
+    plt.ylabel('Peaks')
+    plt.savefig(image_filename_write)
+    # plt.show()
+
+
+fft_module_merged(filename_read, column_header, colors)
